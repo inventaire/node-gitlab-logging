cc36e0938730e434034fc5d4d43d72b25573d0de
maxlath <k@maxlath.eu>
2017-02-02 12:38:19 +0100

    issue_data: allow to pass a context object

--- src/issue_data.js
+++ src/issue_data.js
@@ -3,9 +3,17 @@ const NS = 'gitlab-logging/issue_data'
 // Process issue data
 module.exports = function (errorData, options, checksum) {
     const FN = '[' + NS + '.__data' + ']'
+
+    const { message, labels, stack, context } = errorData
+
+    let description = '```\n' + stack + '```'
+    if (context) {
+      description += '```\n' + JSON.stringify(context, null, 2) + '```'
+    }
+
     return {
-        title: `${errorData.message} [${checksum}]`,
-        description: '```javascript\n' + errorData.stack + '\n```',
-        labels: errorData.labels
+        title: `${message} [${checksum}]`,
+        description,
+        labels
     }
 }
